4c4ada2bfa296e4f1176ec5607f58516e060ce3a
User <User@WRSD-LD309-026.dut.ac.za>
2024-11-12 10:06:33 +0200

    script.js, app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const { render } = require('ejs');
 const express = require('express');
 const app = express();
 const path = require('path');
-const { generateCards } = require('./games')
+
 
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -12,6 +12,21 @@ app.get("/", (req, res) => {
     const cards = generateCards()
     res.render("index", {cards})
 })
+
+function generateCards(){
+    const cardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
+    const cards = []
+
+    cards.push({ id: value, value, matched: false})
+    cards.push({ id: value, value, matched: false})
+
+    for (let i = cards.length -1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        [cards[i], cards[j]] = [cards[j], cards[i]]
+
+    }
+    return cards
+}
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- public/script.js
+++ public/script.js
@@ -1,18 +1,38 @@
-const { reset } = require("nodemon")
-
 let flippedCards = []
-let matchedCards = []
+let matchedCards = 0
 
-const cards = document.querySelectorAll(".card")
+function flipCard(index){
+const card = document.getElementById(`card-${index}`)
+card.classList.add("flipped")
 
-cards.forEach( card => {
-     card.addEventListener("click", () => flipCard(card))
-})
+flippedCards.push(card)
 
-document.getElementById("reset-btn").addEventListener("click", resetGame)
+if (flippedCards.length === 2) {
+    const card1= flippedCards[0]
+    const card2= flippedCards[1]
 
-function flipCard(card){
-    if (flippedCards.length < 2 && !card.classList.contains("flipped") && !matchedCards.includes(card)){
-        card.add
+    const card1Value = card1.getAttribute("data-card-id")
+    const card2Value = card2.getAttribute("data-card-id")
+
+    if (card1Value === card2Value) {
+        matchedCards++;
+        if (matchedCards === 18) {
+            alert("Well done! You won the game!")
+        }
+        else {
+            setTimeout(() => {
+                card1.classList.remove("flipped")
+                card2.classList.remove("flipped")
+            }, 1000)
+
+        }
+        flippedCards = []
+
     }
 }
+function resetGame() {
+    window.location.reload()
+}
+}
+
+
