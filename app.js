b6f92b4f54fdbbaeeba92c611771da02147b5164
User <User@WRSD-LD309-026.dut.ac.za>
2024-11-12 12:12:21 +0200

    index.ejs, app.js updated

--- app.js
+++ app.js
@@ -13,20 +13,28 @@ app.get("/", (req, res) => {
     res.render("index", {cards})
 })
 
-function generateCards(){
-    const cardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
-    const cards = []
-
-    cards.push({ id: value, value, matched: false})
-    cards.push({ id: value, value, matched: false})
-
-    for (let i = cards.length -1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
-        [cards[i], cards[j]] = [cards[j], cards[i]]
-
+function generateCards() {
+    const cardValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    let cards = [];
+    
+    // Create 18 pairs of cards
+    cardValues.forEach(function(value) {
+        cards.push({ id: value, value, matched: false });
+        cards.push({ id: value, value, matched: false });
+    });
+    
+    // Shuffle the cards using the Fisher-Yates algorithm
+    for (let i = cards.length - 1; i > 0; i--) {
+        // Declare 'j' inside the loop
+        const j = Math.floor(Math.random() * (i + 1));
+        
+        // Swap the cards at index i and j
+        [cards[i], cards[j]] = [cards[j], cards[i]];
     }
-    return cards
+    
+    return cards;
 }
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
